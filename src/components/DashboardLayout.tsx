325080af973bf6dc1c9527f0f9db40d5363d96af
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2023-01-23 21:32:58 +0100

    feat(DashboardLayout): :sparkles: make dashboard bar sticky

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -30,7 +30,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <AppBar position="static">
+      <AppBar position="sticky">
         <Container maxWidth="xl" sx={{ minHeight: "100%" }}>
           <Toolbar disableGutters>
             <Grid container>
