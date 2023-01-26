b8d7a5db3625d02101013457729905a4fcf5e55a
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2023-01-27 00:36:24 +0100

    feat(env): :sparkles: support env variable for api base url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000/";
 
 export const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
 });
 
 api.defaults.headers.common["Content-Type"] = "application/json";
